dcca4337a09dc8611f34df409b1c0656e1cabca2
Anton Hansson <hansson@google.com>
2018-02-23 12:57:51 +0000

    caf/telephony: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: I8d81c499b1db80496ce3b3b1bdf2301810d3542c
    Merged-In: I4233b9091d9066c4fa69f3d24aaf367ea500f760
    
    # Conflicts:
    #	ims/ConfURIDialer/Android.mk
    #	ims/InCallUi/Android.mk

--- ims/ConfURIDialer/Android.mk
+++ ims/ConfURIDialer/Android.mk
@@ -7,8 +7,10 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := ConfURIDialer
-LOCAL_SDK_VERSION := system_current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_CERTIFICATE := platform
+
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
--- ims/InCallUi/Android.mk
+++ ims/InCallUi/Android.mk
@@ -7,10 +7,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := qtiImsInCallUi
-LOCAL_SDK_VERSION := system_current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_CERTIFICATE := platform
 
 LOCAL_MODULE_OWNER := qti
+
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
