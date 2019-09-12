2ebe17cf6df4f74c7ab76f5a911d75d651a9e6b6
Marko Man <darkobas@gmail.com>
2019-09-12 20:10:11 +0200

    Merge tag 'LA.UM.8.1.r1-08700-sm8150.0' into HEAD
    "LA.UM.8.1.r1-08700-sm8150.0"
    
    # Conflicts:
    #	ims/Android.mk
    #	internal/Android.bp

--- internal/src/org/codeaurora/internal/NetworkCallbackBase.java
+++ internal/src/org/codeaurora/internal/NetworkCallbackBase.java
@@ -60,14 +60,6 @@ public class NetworkCallbackBase extends INetworkCallback.Stub {
     }
 
     @Override
-    public void onSignalStrength(int slotId, Token token, Status status, SignalStrength
-            signalStrength) throws RemoteException {
-        Log.d(TAG,
-                "onSignalStrength: slotId = " + slotId + " token = " + token + " " + "status"
-                        + status + " signalStrength = " + signalStrength);
-    }
-
-    @Override
     public void onAnyNrBearerAllocation(int slotId, Token token, Status status,
             BearerAllocationStatus bearerStatus) throws RemoteException {
         Log.d(TAG,
@@ -76,11 +68,19 @@ public class NetworkCallbackBase extends INetworkCallback.Stub {
     }
 
     @Override
+    public void onSignalStrength(int slotId, Token token, Status status, SignalStrength
+            signalStrength) throws RemoteException {
+        Log.d(TAG,
+                "onSignalStrength: slotId = " + slotId + " token = " + token + " " + "status"
+                        + status + " signalStrength = " + signalStrength);
+    }
+
+    @Override
     public void onUpperLayerIndInfo(int slotId, Token token, Status status,
             UpperLayerIndInfo uilInfo) throws RemoteException {
         Log.d(TAG,
                 "onUpperLayerIndInfo: slotId = " + slotId + " token = " + token + " " +
-                        "status" + status + " UpperLayerIndInfo = " + uilInfo);
+                "status" + status + " UpperLayerIndInfo = " + uilInfo);
     }
 
     @Override
@@ -88,7 +88,7 @@ public class NetworkCallbackBase extends INetworkCallback.Stub {
             nrConfigType) throws RemoteException {
         Log.d(TAG,
                 "on5gConfigInfo: slotId = " + slotId + " token = " + token + " " + "status"
-                        + status + " NrConfigType = " + nrConfigType);
+                + status + " NrConfigType = " + nrConfigType);
     }
 
     @Override
